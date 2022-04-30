c6f281e5be1bc05bbe82b8eee9a27c7cf10d569e
JunCo <36791397+jkjun1234@users.noreply.github.com>
2022-05-01 03:22:01 +0900

    자산 입력전달 Form을 만듬

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import {useState, useEffect} from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
-  const [account, setAccount] = useState();
+  const [account, setAccount] = useState(0);  // 나의 자산
+  const [selectedName, setSelectedName] = useState(""); //선택한 코인이름
+  const [selectedValue, setSelectedValue] = useState(''); //코인 가격
+  const [viewcell, setViewcell] = useState('');     // 얼마 살수 있는지 알려주는 view
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers?limit=20")
       .then((response) => response.json())
@@ -13,32 +16,55 @@ function App() {
         console.log(json);
       });
   }, []);
+  const selectOnchange = (event) => {
+    let selectedStr = event.target.value.split('_');    // value 값 name과 quotes 값 나누기
+    
+    setSelectedValue(selectedStr[1]);
+    setSelectedName(selectedStr[0]); 
+  };
+  const accountInput = (event) => {
+    //나의 자산 입력
+    setAccount(event.target.value);
+    console.log(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(account);
+    if(account === 0 && selectedName === "") {
+      return setViewcell("코인을 선택하고 금액을 넣어주세요");
+    }
 
+    return setViewcell(selectedName + "을 ~로~ 살수있습니다.");
+  };
+ 
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
 
       {/**Loding 중이 아니면 아무것도 보여주지 않음*/}
       {loading ? <strong>Loading...</strong>: 
-        <select>
+        <select onChange={selectOnchange} value={selectedName+'_'+selectedValue}> 
+          {/**select box 값 가져오기 */}   
           {/**.map() argment에 key값과 value 값이 필요한데 여기선 key값이 사용되지 않음
            * API의 json 코드에서 key 값이 존재하기 때문이다.
           */}
           {coins.map((coin) => (
-            <option>
+            <option value={coin.name+'_'+coin.quotes.USD.price} >
               {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
             </option>
-          ))}
-
-          {/** 코드 챌린지 내용
-           * 내가 가지고있는 달러를 가지고 
-           * 얼마의 코인을 구매 가능한지 변환하는 앱을 만들어보라
-           * <li>태그 대신 <select> 태그를 사용할 것
-          */}
+          ))
+          }
         </select>
         }
-        <h2>this 비트 코인을 ~개 살수 있습니다.</h2> 
-        <input placeholder="please input your account"></input>
+        {/**account 입력*/}
+        <h1>{viewcell}</h1>
+        <form onSubmit={onSubmit}>
+          <input placeholder="please input your account"
+            onChange={accountInput}
+            type='number'
+          ></input>
+          <button>변환!</button>
+        </form>
     </div>
   );
 }
